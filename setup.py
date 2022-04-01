8106b9a221bd58e18fe7d8862ec1be16d9e82a2d
Michał Murawski <mmurawski777@gmail.com>
2022-04-01 15:58:20 +0200

    Updated exception message formating

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*parts):
 
 setup(
     name='pynamodb_utils',
-    version='1.1.2',
+    version='1.1.3',
     author="Michal Murawski",
     author_email="mmurawski777@gmail.com",
     description="Utilities package for pynamodb.",
