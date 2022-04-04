f3f81e95775d16068eef6596173b509c535dac2c
Michal Murawski <mmurawski777@gmail.com>
2022-04-04 17:47:16 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*parts):
 
 setup(
     name='pynamodb_utils',
-    version='1.1.3',
+    version='1.1.4',
     author="Michal Murawski",
     author_email="mmurawski777@gmail.com",
     description="Utilities package for pynamodb.",
